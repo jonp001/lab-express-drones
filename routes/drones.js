f8914789e7719dc458aa0c184e8c021e24c64a04
Jon Perez <jonperez01@outlook.com>
2023-05-18 18:11:30 -0400

    finished lab, but still getting strictQuery warning even after typing in the suggested fix from ternminal

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,102 @@
 const express = require('express');
 const router = express.Router();
 
+
 // require the Drone model here
+const Drone= require('../models/Drone.model.js')
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  async function findAllDronesFromDb() {
+    try{
+      let allDronesFromDb= await Drone.find();
+      res.render('../views/drones/list.hbs', {drones: allDronesFromDb});
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+  findAllDronesFromDb();
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  
+    res.render("../views/drones/create-form.hbs");
 });
+
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed} = req.body;
+
+
+  async function createDroneInDb(){
+    try{
+      let createdDrone= await Drone.create({name, propellers, maxSpeed});
+      res.redirect("/drones");
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+  createDroneInDb();
 });
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params;
+  console.log(id);
+
+  async function updateDrone() {
+
+    try{
+      let updatedDrone= await Drone.findById(id);
+      res.render("drones/update-form.hbs", {drone: updatedDrone});
+
+    } catch(error){
+      console.log(error)
+  }
+}
+  updateDrone();
+
 });
+
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+ const {id} = req.params
+ const {name, propellers, maxSpeed} = req.body;
+
+ async function updateADroneFromDb() {
+  try{
+    let updatedDroneDb= await Drone.findByIdAndUpdate(id, {name, propellers, maxSpeed}, {new: true})
+    res.redirect("/drones");
+
+  } catch(error) {
+    console.log(error)
+  }
+
+  }
+ updateADroneFromDb();
 });
+
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+const {DroneId} = req.params;
+
+async function deleteADroneFromDb() {
+  try{
+    let deletedDrone = await Drone.findByIdAndDelete(id);
+    res.redirect("/drones");
+  }
+  catch(err) {
+    console.log(err)
+  }
+}
+deleteADroneFromDb();
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,47 @@
 // Iteration #1
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', true)
+
+const Drone = require('../models/Drone.model.js')
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+
+
+const drones = [
+
+{ 
+    name: "Creeper XL 500",
+    propellers: 3,
+    maxSpeed: 12
+},
+
+{
+    name: "Racer 57",
+    propellers: 4,
+    maxSpeed: 20
+
+},
+
+{
+    name: "Courier 3000i",
+    propellers: 6,
+    maxSpeed: 18
+}
+];
+
+async function insertDrones() {
+    try {
+        let db= await mongoose.connect(MONGO_URI);
+        console.log(`Connected to Mongo Database via : ${db.connections[0].name}`);
+
+        let dronesCreated = await Drone.create(drones);
+        console.log(`Created Drones: ${dronesCreated.length} drones!`)
+        await mongoose.connection.close();
+        }
+        catch (error) {
+            console.log(" An error occured upon creation", error)
+        }
+    } 
+    
+    insertDrones();
